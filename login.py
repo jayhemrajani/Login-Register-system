d08c811cfb75e51ff2c7ba4688e9fcd644c65960
jayhemrajani <143853771+jayhemrajani@users.noreply.github.com>
2025-04-13 18:51:42 -0400

    Update login.py
    Added date/timestamps, ability to see if another account with the same username exists already, eliminates certain characters from being put in usernames.

--- login.py
+++ login.py
@@ -1,14 +1,45 @@
-def register():
+from datetime import datetime
+
+def register(found):
     username = input("Enter your username: ")
+    for char in username:
+        if char == ';': 
+            print("Your username can't contain the following character: ;")
+            found = False
+            return False
+    with open("accountdetails.txt", "r") as file:
+        for line in file:
+            saved_username, saved_password = line.strip().split(";")
+            if username == saved_username:
+                print("An account with this username already exists!")
+                return False
+    file.close()
+
     password = input("Enter your password: ")
+    for char in password:
+        if char == ';': 
+            print("Your username can't contain the following character: ;")
+            found = False
+            return False
+        
+    found = True
     file = open("accountdetails.txt", "a")
     file.write(username + ";" + password + "\n")
     file.close()
 
-def login():
+   
+    if found == True:
+        print("Account registed successfully. Welcome " + username + "!")
+    else:
+        print("Error registering account.")
+    return found
+
+def login(found):
+    
     username = input("Enter your username: ")
     password = input("Enter your password: ")
     found = False
+
 
     with open("accountdetails.txt", "r") as file:
         for line in file:
@@ -19,17 +50,38 @@ def login():
             
     if (found == True):
         print("Login Successful!")
+        return True
     else:
         print("Invalid Username or Password!")
+        return False
 
 def main():
-    option = input("Enter an option: \n1. Register\n2. Login\n")
-    if (option == '1'):
-        register()
-    elif (option == '2'):
-        login()
-    else:
-        print("That is not a valid option!")
 
+
+    found = False
+    while (found == False):
+        time = datetime.now()
+        
+        option = input("Enter an option: \n1. Register\n2. Login\n3. Exit Program\n")
+        if (option == '1'):
+            found = register(found)
+            if found == True:
+                current_time = time.strftime("%H:%M:%S")
+                current_date = time.strftime("%m/%d/%Y")
+                print("Register timestamp: " + current_time + "\nRegister date: " + current_date)
+        elif (option == '2'):
+            found = login(found)
+            if found == True:
+                current_time = time.strftime("%H:%M:%S")
+                current_date = time.strftime("%m/%d/%Y")
+                print("Login timestamp: " + current_time + "\nLogin date: " + current_date)
+        elif(option == '3'):
+            print("")
+            exit()
+        else:
+            print("That is not a valid option!")
+            
+
+    
 if __name__ == '__main__':
     main()
